113d35c9a70ab98905f8455982bd6f8f66b46499
GeorgiyII <gosha1104@gmail.com>
2020-06-02 15:38:25 +0300

    added spycloud mock endpoint

--- api/spycloud.py
+++ api/spycloud.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify
+from werkzeug.exceptions import TooManyRequests
 
 spycloud_api = Blueprint('spycloud', __name__)
 
@@ -7,7 +8,7 @@ spycloud_api = Blueprint('spycloud', __name__)
 def spy_data():
     data = {
         "cursor": "",
-        "hits": 33,
+        "hits": 2,
         "results": [
             {
                 "domain": "example.org",
@@ -94,7 +95,37 @@ def spy_catalog_17674():
 def spy_catalog_17551():
     catalog = {
         "cursor": "",
-        "hits": 0,
-        "results": []
+        "hits": 1,
+        "results": [
+            {
+                "confidence": 3,
+                "description": "AzorUlt is a lesser known credential stealing botnet, also known as crimeware. This malware steals data from infected computers via web browsers and protected storage. Once infected, the computer sends the stolen data to a bot command and control (C&C) server, where the data is stored. Any credentials from this source can be assumed to already be in the hands of threat actors, and should be changed immediately.",
+                "title": "Azorult Botnet",
+                "type": "PRIVATE",
+                "acquisition_date": "2020-05-27T00:00:00Z",
+                "site": "n/a",
+                "spycloud_publish_date": "2020-05-28T00:00:00Z",
+                "site_description": "The AzorUlt Botnet is a lesser-known botnet that is mainly used for stealing credentials.",
+                "uuid": "8f8a4738-7068-4666-882b-793d485fd9da",
+                "num_records": 1530174,
+                "id": 17674,
+                "assets": {
+                    "username": 644708,
+                    "infected_path": 23558,
+                    "infected_time": 1530174,
+                    "user_browser": 1530174,
+                    "ip_addresses": 1530174,
+                    "target_url": 1530174,
+                    "password": 1530174,
+                    "email": 885466,
+                    "infected_machine_id": 1530174
+                }
+            }
+        ]
     }
     return jsonify(catalog)
+
+
+@spycloud_api.route('/spycloud/breach/data/emails/test@test.com ', methods=['GET'])
+def spy_data():
+    raise TooManyRequests
